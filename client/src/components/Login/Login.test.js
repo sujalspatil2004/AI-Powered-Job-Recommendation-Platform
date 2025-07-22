eb66628b62b320f6b42933d4564bff9248640ce2
Harshavardhan <pharshavardhanreddy81166@gmail.com>
2025-07-22 23:13:45 +0530

    fix(test): add MemoryRouter import for Login component test

--- client/src/components/Login/Login.test.js
+++ client/src/components/Login/Login.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Login from '../Login/index.jsx';
 
 describe('Login Component', () => {
